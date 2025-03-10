f63f57b8f6bdf58ebe81e02fff248a2d0f5b4fd6
ArtDani10v <artdanilovru@gmail.com>
2025-03-10 10:29:16 +0700

    Добавлена функция блюд

--- file_dz_1.py
+++ file_dz_1.py
@@ -31,8 +31,33 @@ def read_recipes(filename):
     return cook_book
 
 
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
+    shop_list = {}
+    
+    # Перебираем каждое блюдо из списка
+    for dish in dishes:
+        # Проверяем, есть ли такое блюдо в cook_book
+        if dish in cook_book:
+            # Перебираем ингредиенты блюда
+            for ingredient in cook_book[dish]:
+                name = ingredient['ingredient_name']
+                measure = ingredient['measure']
+                quantity = ingredient['quantity'] * person_count
+                
+                # Если ингредиент уже есть в списке, суммируем количество
+                if name in shop_list:
+                    shop_list[name]['quantity'] += quantity
+                else:
+                    shop_list[name] = {'measure': measure, 'quantity': quantity}
+        else:
+            print(f"Блюдо '{dish}' отсутствует в кулинарной книге.")
+    
+    return shop_list
 
 
 filename = "recipes.txt"  # Имя файла относительно текущей директории
 cook_book = read_recipes(filename)
 print(cook_book)
+input('Enter')
+result = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2, cook_book)
+print(result)
